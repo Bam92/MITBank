635e36481922bd3280e83982142d5e0d49b912e2
Abel L Mbula <abelmbula@protonmail.com>
2022-02-05 18:51:17 +0200

    feat: allow user to withdrow + format currency view

--- MITBank/src/com/abelmbula/Bank.java
+++ MITBank/src/com/abelmbula/Bank.java
@@ -1,5 +1,6 @@
 package com.abelmbula;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 public class Bank {
@@ -13,14 +14,51 @@ public class Bank {
 		clients.add(newClient);
 	}
 	
+	/**
+	 * Do a deposit to an account
+	 * @param id client id
+	 * @param amount deposit amount
+	 */
+	public void deposit(int id, double amount) {
+		try {
+			Client currentClient = clients.get(id - 1);			
+			double currentSolde = currentClient.getSolde();
+			currentClient.setsolde(currentSolde + amount);
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("Client does not exist. Try again, please!");
+		}
+	}
+	
+	/**
+	 * Withdrow money
+	 * @param id client id
+	 * @param amount withdrow amount
+	 */
+	public void withdrow(int id, double amount) {
+		try {
+			Client currentClient = clients.get(id - 1);			
+			double currentSolde = currentClient.getSolde();
+			
+			if (currentSolde < amount) System.out.println("Votre solde actuel ne permet pas de faire le retrait de ce montant");
+			else 
+				currentClient.setsolde(currentSolde - amount);
+		} catch (IndexOutOfBoundsException e) {
+			System.err.println("Client does not exist. Try again, please!");
+		}
+	}
+	
 	public void display() {
 		System.out.println("Liste des clients: ");
 	
 	for (int i = 0; i < clients.size(); i++) {
-		System.out.println("Client:");
+		System.out.println("Info client:");
 		System.out.println(
-				"Numero " + (i + 1) + " " + clients.get(i).getFirstName() + " " + clients.get(i).getLastName().toUpperCase() + " " + clients.get(i).getSolde()
-				);
+				"Numero: " + (i + 1) + " " + 
+				"\n" + "Prenom: " + clients.get(i).getFirstName() + " " + 
+				"\n" + "Nom: " + clients.get(i).getLastName().toUpperCase() + " " + 
+				"\n" + "Solde: " + NumberFormat.getCurrencyInstance().format(clients.get(i).getSolde())
+		);
+		System.out.println("---------");
 	}
 	}
 }
--- MITBank/src/com/abelmbula/Main.java
+++ MITBank/src/com/abelmbula/Main.java
@@ -4,17 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		Client abel = new Client("Abel", "Mbula", 0d);
-		Client angel = new Client("Angel", "Mbula", 20d);
-		Client jean = new Client("Jean", "Mbula", 0d);
-		Client aurelie = new Client("aurelie", "Mbula", 20d);
-		Client joseph = new Client("Joseph", "Mbula", 20d);
+		Client patience = new Client("Patience", "Kavira", 20d);
+		Client jean = new Client("Jean", "LaCombe", 0d);
+		Client aurelie = new Client("Aurelie", "Mambo", 20d);
+		Client joseph = new Client("Joseph", "Lomata", 20d);
 		
 		Bank account = new Bank();
-		account.addClient(angel);
+		account.addClient(patience);
 		account.addClient(abel);
 		account.addClient(jean);
 		account.addClient(aurelie);
 		account.addClient(joseph);
+		
+		System.out.println("*************************************************************");
+		System.out.println("BIENVENUE CHEZ MITBANK !");
+		System.out.println("*************************************************************");
+		account.display();
+		
+		// deposit
+		account.deposit(1, 24.0);
+		account.deposit(3, 14.0);
+		
+		//withdrow
+		account.withdrow(3, 5.5);
 		
 		account.display();
 		
